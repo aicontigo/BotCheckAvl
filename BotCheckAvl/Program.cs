3a8f20bdc9e8c2d9f5a9f6e3a145f314a6e40505
Alexey Bubley <alexey.bubley@discovercarhire.com>
2025-06-22 14:10:30 +0200

    Simplify initialization

--- BotCheckAvl/Program.cs
+++ BotCheckAvl/Program.cs
@@ -2,19 +2,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 class Program
 {
     public static async Task Main(string[] args)
     {
         using IHost host = Host.CreateDefaultBuilder(args)
-            .ConfigureLogging((context, logging) =>
-            {
-                var logLevel = context.Configuration["AppSettings:LogLevel"];
-                logging.ClearProviders();
-                logging.AddConsole();
-                logging.SetMinimumLevel(ParseLogLevel(logLevel));
-            })
             .ConfigureServices((context, services) =>
             {
                 services.Configure<TgBotSettings>(context.Configuration.GetSection("TgBot"));
@@ -26,27 +20,7 @@ class Program
         var logger = host.Services.GetRequiredService<ILogger<Program>>();
         var env = host.Services.GetRequiredService<IHostEnvironment>();
 
-        var featureXEnabled = config.GetValue<bool>("AppSettings:FeatureXEnabled");
+        logger.LogInformation($"Starting {Assembly.GetExecutingAssembly().GetName().Name} in {env.EnvironmentName} environment");
 
-        logger.LogInformation($"Environment: {env.EnvironmentName}");
-        logger.LogInformation($"FeatureXEnabled: {featureXEnabled}");
-
-        if (featureXEnabled)
-            logger.LogDebug("Feature X is enabled!");
-        else
-            logger.LogWarning("Feature X is disabled.");
-
-        await host.RunAsync();
     }
-
-    private static LogLevel ParseLogLevel(string? level) => level?.ToLower() switch
-    {
-        "trace" => LogLevel.Trace,
-        "debug" => LogLevel.Debug,
-        "information" => LogLevel.Information,
-        "warning" => LogLevel.Warning,
-        "error" => LogLevel.Error,
-        "critical" => LogLevel.Critical,
-        _ => LogLevel.Information
-    };
 }
