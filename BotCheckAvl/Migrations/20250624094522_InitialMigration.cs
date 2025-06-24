5ecf1c7b9ac75a4b4ab027dde6cbf6e80eb89437
Alexey Bubley <alexey.bubley@discovercarhire.com>
2025-06-24 11:49:28 +0200

    Add initial migration

---
+++ BotCheckAvl/Migrations/20250624094522_InitialMigration.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BotCheckAvl.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TelegramUserId = table.Column<long>(type: "INTEGER", nullable: false),
+                    Username = table.Column<string>(type: "TEXT", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MonitoredServices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Url = table.Column<string>(type: "TEXT", nullable: false),
+                    IsEnabled = table.Column<bool>(type: "INTEGER", nullable: false),
+                    TimeoutMs = table.Column<int>(type: "INTEGER", nullable: false),
+                    MaxRetryes = table.Column<int>(type: "INTEGER", nullable: false),
+                    ResponseTimeWarningMs = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsLive = table.Column<bool>(type: "INTEGER", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MonitoredServices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MonitoredServices_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PingHistories",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Timestamp = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    IsUp = table.Column<bool>(type: "INTEGER", nullable: false),
+                    ResponseTimeMs = table.Column<int>(type: "INTEGER", nullable: true),
+                    ResponseCode = table.Column<string>(type: "TEXT", nullable: true),
+                    MonitoredServiceId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PingHistories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PingHistories_MonitoredServices_MonitoredServiceId",
+                        column: x => x.MonitoredServiceId,
+                        principalTable: "MonitoredServices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MonitoredServices_UserId",
+                table: "MonitoredServices",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PingHistories_MonitoredServiceId",
+                table: "PingHistories",
+                column: "MonitoredServiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_TelegramUserId",
+                table: "Users",
+                column: "TelegramUserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PingHistories");
+
+            migrationBuilder.DropTable(
+                name: "MonitoredServices");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
